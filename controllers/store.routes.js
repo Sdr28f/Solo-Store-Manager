36fed7cacc11cfaf018f27bd1275104a4765eeab
syed danyal <syeddanyal28f@gmail.com>
2026-01-21 18:17:50 +0300

    finish delete and update on store model

--- controllers/store.routes.js
+++ controllers/store.routes.js
@@ -29,6 +29,28 @@ router.get('/:id',async(req,res)=>{
     res.render('store-details.ejs',{foundStore:foundStore})
 })
 
+/// DELETE
+router.post('/delete/:id',async(req,res)=>{
+    const deleteStore = await Store.findByIdAndDelete(req.params.id)
+    res.redirect('/stores');
+});
+
+/// UPDATE
+
+router.get('/:id/edit',async(req,res)=>{
+    const editStore = await Store.findById(req.params.id);
+    res.render('edit-store.ejs',{editStore:editStore});
+})
+
+router.post('/update/:id',async(req,res)=>{
+    if(req.body.isActive === 'on'){
+        req.body.isActive = true;
+    }else{
+        req.body.isActive = false;
+    }
+    await Store.findByIdAndUpdate(req.params.id,req.body);
+    res.redirect('/stores/' + req.params.id );
+})
 
 
 
